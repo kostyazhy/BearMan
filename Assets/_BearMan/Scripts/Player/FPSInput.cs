460ad8e7fba41f22193d5d6b61c7cc54522be1a5
kostya_zhy <kostyazhy@gmail.com>
2020-09-17 11:40:32 +0700

    Add jump

--- Assets/_BearMan/Scripts/Player/FPSInput.cs
+++ Assets/_BearMan/Scripts/Player/FPSInput.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
-    public float gravity = 9.8f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+    private float _vertSpeed;
     private CharacterController _charController;
+
     void Start()
     {
         _charController = GetComponent<CharacterController>();
@@ -20,7 +25,19 @@ public class FPSInput : MonoBehaviour
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
-        movement.y = gravity;
+        if (_charController.isGrounded) {
+            if (Input.GetButtonDown("Jump")) {
+                _vertSpeed = jumpSpeed;
+            } else {
+                _vertSpeed = minFall;
+            }
+        } else {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocity) {
+                _vertSpeed = terminalVelocity;
+            }
+        }
+        movement.y = _vertSpeed;
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
         _charController.Move(movement);
